d5d0676b192b9edcf7f98386454a20537da213cb
rzzza_rehan <rhn.raza121@gmail.com>
2024-10-26 13:03:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const usersRoutes = require('./routes/users');
+
+const app = express();
+
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+
+  next();
+});
+
+app.use("/api/users", usersRoutes);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./app');
+// const debug = require('debug')("node-angular");
+
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, ()=> {
+    console.log("server is listening");
+});
